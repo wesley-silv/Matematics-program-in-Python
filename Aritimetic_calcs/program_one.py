eda77f5bc1cfd92ba0caf275d8143ca75a3c335b
Wesley Silva C <wesleysilvaconceicao@outlook.com>
2023-02-28 21:01:29 -0300

    matematics_program intanciado e funcionando em todos subprogramas

---
+++ Aritimetic_calcs/program_one.py
@@ -0,0 +1,16 @@
+print("Criando programas com python no conhecimento da linguagem, o mesmo deve ser usando para resolver problemas diversos.")
+
+# Programa de login de usuários em um sistema.
+print("Login in the Web Page")
+users_cadastred = [
+  "wesleysilv23@gmail.com", "melinysara6@gmail.com"
+]
+name_user = print(str(input("Digite o nome do usuário para ter acesso ao programa: ")))
+
+if(name_user in users_cadastred):
+  print("Usuário cadastrado, acesso permitido")
+else:
+  print("Usuário não cadastrado, acesso não permitido: ")
+
+  # pensar em como resolver esse problema do programa, pois a verificação não esta encontrando os usuários cadastrados.
+  # pesquisar na documentação do Python.
